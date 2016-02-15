6d23fcb67b1fcf9597a27aee8c8a1c19142c1c78
frank finger <franklinfinger@franks-MacBook-Pro.local>
2016-02-15 17:14:17 -0500

    clean up

--- time-to-read.js
+++ time-to-read.js
@@ -1,17 +1,3 @@
-// var readTime function() {
-//   "use strict";
-//   var wordsPm = {
-//     wpm: 230
-// };
-//
-// function setWpm(wpm) {
-//   wordsPm.wpm = wpm;
-// }
-//
-// function getWpm() {
-//   return wordsPm.wpm;
-//
-
 var wordText = "";
 var pTags = document.getElementsByTagName("p");
 var arr = [].slice.call(pTags);
@@ -31,17 +17,3 @@ countWords();
 
 var wPm= document.getElementsByClassName("reading-time");
 console.log(wPm[0].innerText = "WPM = 5.326");
-
-
-// var bodyContent = document.getElementy('content');
-// var bodyUl = document.createElement('ul');
-// bodyContent.appendChild(bodyUl);
-//
-// redditObj.data.children.forEach(function(item,idx,arr) {
-//   var appendToPage = document.createElement('li');
-//   var scoreSpan = document.createElement('span');
-//   scoreSpan.innerText = item.data.score
-//   appendToPage.innerText = item.data.title;
-//   appendToPage.appendChild(scoreSpan);
-//   bodyUl.appendChild(appendToPage);
-// });
